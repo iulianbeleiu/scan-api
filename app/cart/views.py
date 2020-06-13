70f51c9aa773d4e8bcc0ca9c8b40f3d5c40242d5
Iulian Beleiu <iulian.beleiu@gmail.com>
2020-06-13 23:42:11 +0300

    generate receipt pdf

--- app/cart/views.py
+++ app/cart/views.py
@@ -1,6 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+
+from django.template.loader import get_template
+from django.http import HttpResponse
+
+from xhtml2pdf import pisa
 
 from .models import CartItem, Cart
 
@@ -41,3 +47,19 @@ class CartViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Return object for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user)
+
+    @action(methods=['GET'], detail=True, url_path='receipt')
+    def receipt(self, request, pk=None):
+        cart = self.get_object()
+        pdf_name = f'receipt-{cart.id}.pdf'
+        template_path = 'receipt.html'
+
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="{pdf_name}"'
+
+        template = get_template(template_path)
+        html = template.render({'cart': cart, 'user': request.user})
+
+        pisa.CreatePDF(html, dest=response)
+
+        return response
