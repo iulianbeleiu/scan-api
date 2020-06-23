91aa2c4093c6d0af89beeee2f8d24a162b5eacb4
Iulian Beleiu <i.beleiu@plan-net.com>
2020-06-23 11:31:56 +0300

    remove tag app

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'core',
     'user',
-    'tag',
     'shop',
     'cart',
 ]
--- app/app/urls.py
+++ app/app/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('api/user/', include('user.urls')),
-    path('api/tag/', include('tag.urls')),
     path('api/shop/', include('shop.urls')),
     path('api/order/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
